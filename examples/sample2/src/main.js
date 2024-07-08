18f5ca33624e6aab7bc9b571e7286ca23291333a
MinhNg <mnguyen402@gatech.edu>
2024-07-07 23:00:55 -0400

    working on createAuth

--- examples/sample2/src/main.js
+++ examples/sample2/src/main.js
@@ -7,10 +7,10 @@ import { createAuth } from "veda-auth-vue";
 createApp(App)
     .use(
         createAuth({
-            clientId: "veda-tk5a3inut1y0ot5rvoc7-10000000",
-            clientSecret: "zQQUrVvsxSEwZhpLBi6nDVGaruULfZ0lhr2td62K",
-            vedaAuthBaseUrl: "http://localhost:8081/api/v1",
-            redirectUri: "http://localhost:5173/callback/",
+            clientId: "",
+            clientSecret: "",
+            vedaAuthBaseUrl: "",
+            redirectUri: "",
         })
     )
     .mount("#app");
